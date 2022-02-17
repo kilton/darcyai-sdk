a11f83a51d78af10ccdd67736e588c266bcb0cb7
Saeid Rezaei Baghbidi <saeid@edgeworx.io>
2022-02-16 20:12:07 -0800

    Moving to darcyai

--- docs.sh
+++ docs.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 from http.server import HTTPServer, CGIHTTPRequestHandler
 
-os.chdir("./api")
+os.chdir("./docs")
 
 port = int(os.getenv("PORT", 8000))
 server_object = HTTPServer(server_address=("", port), RequestHandlerClass=CGIHTTPRequestHandler)
